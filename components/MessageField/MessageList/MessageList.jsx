3f6802b5ae4d198fe02a85604cb4586eb0732312
misha <mixan_45@mail.ru>
2022-10-20 22:10:01 +0300

    22/10.12

--- components/MessageField/MessageList/MessageList.jsx
+++ components/MessageField/MessageList/MessageList.jsx
@@ -7,9 +7,12 @@ import {getImage} from "../../../actions/auth";
 
 const MessageList = ({e, id, index}) => {
 
+
     return (
             <span key={index} className={(e.id_Adder === id)? 'mess_textBlock_reverse message_size' : 'mess_textBlock message_size hisMessage'}>
             {e.text}
+                <span className={(e.id_Adder === id)? 'mess_time_reverse' : 'mess_time'}>{e.createdAt.slice(11,13)}:{e.createdAt.slice(14,16)}</span>
+                <span className={(e.id_Adder === id)? 'month_time_reverse' : 'month_time'}>{e.createdAt.slice(11,13)}:{e.createdAt.slice(14,16)}</span>
         </span>
     )
 
